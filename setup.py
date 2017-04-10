e62be9050ff46d54e2e036449ff1961495c4ad74
Javier Tarazaga <javier.tarazaga@valtech.se>
2017-04-10 14:02:44 +0200

    Reverter to original form except change

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ version = __import__('background_task').__version__
 classifiers = [c for c in open('classifiers').read().splitlines() if '#' not in c]
 
 setup(
-    name='snabb-django-background-tasks',
+    name='django-background-tasks',
     version=version,
     description='Database backed asynchronous task queue',
     long_description=codecs.open('README.md', encoding='utf-8').read(),
     author='arteria GmbH, John Montgomery',
     author_email='admin@arteria.ch',
-    url='https://github.com/SnabbHQ/django-background-tasks',
+    url='http://github.com/arteria/django-background-tasks',
     license='BSD',
     packages=find_packages(exclude=['ez_setup']),
     include_package_data=True,
