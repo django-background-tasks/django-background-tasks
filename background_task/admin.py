84f7e5c657fb7ce810a2bccd105c452b9cee7b16
Hodossy, Szabolcs <szabolcs.hodossy@continental-corporation.com>
2020-09-01 16:13:34 +0200

    feat: admin restart action

--- background_task/admin.py
+++ background_task/admin.py
@@ -16,16 +16,24 @@ def dec_priority(modeladmin, request, queryset):
         obj.save()
 dec_priority.short_description = "priority -= 1"
 
+def restart_completed(modeladmin, request, queryset):
+    for obj in queryset:
+        obj.create_restarted_task()
+restart_completed.short_description = "Restart selected completed tasks"
+
 class TaskAdmin(admin.ModelAdmin):
     display_filter = ['task_name']
     search_fields = ['task_name', 'task_params', ]
     list_display = ['task_name', 'task_params', 'run_at', 'priority', 'attempts', 'has_error', 'locked_by', 'locked_by_pid_running', ]
+    list_filter = ['task_name', 'has_error']
     actions = [inc_priority, dec_priority]
 
 class CompletedTaskAdmin(admin.ModelAdmin):
     display_filter = ['task_name']
     search_fields = ['task_name', 'task_params', ]
     list_display = ['task_name', 'task_params', 'run_at', 'priority', 'attempts', 'has_error', 'locked_by', 'locked_by_pid_running', ]
+    list_filter = ['task_name', 'has_error']
+    actions = [restart_completed]
 
 
 admin.site.register(Task, TaskAdmin)
