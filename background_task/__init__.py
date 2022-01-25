90594bbd325e31c1e1a526823f9868e7b6dbf505
s-nikhil-lilly <94434007+s-nikhil-lilly@users.noreply.github.com>
2022-01-25 16:34:54 +0530

    Update __init__.py
    Support for Django 3.2 
    
    Removed "default_app_config" Django version greater or equal to Django 3.2

--- background_task/__init__.py
+++ background_task/__init__.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
+import django
 __version__ = '1.2.5'
 
-default_app_config = 'background_task.apps.BackgroundTasksAppConfig'
-
+if django.VERSION < (3, 2):
+    default_app_config = 'background_task.apps.BackgroundTasksAppConfig'
+    
 def background(*arg, **kw):
     from background_task.tasks import tasks
     return tasks.background(*arg, **kw)
